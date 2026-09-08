47623c1bda683b19e5948efd0c4bb61517d8cba1
Ho Duy Nhat <hdnhat2005@gmail.com>
2026-09-08 15:28:35 +0700

    chore(setup): set up project

---
+++ src/test/java/com/taca/paymentwallet/PaymentWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taca.paymentwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
